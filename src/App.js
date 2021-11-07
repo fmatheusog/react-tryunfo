c7c13348709949cf0f4df83c667c365df4060b81
Felipe Matheus <fmatheusog@gmail.com>
2021-11-07 08:14:38 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { React, Component } from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
+
+import './styles/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="card-preview">
         <Form />
+        <Card />
       </div>
     );
   }
---
+++ src/components/Card.js
@@ -0,0 +1,60 @@
+import { React, Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Card extends Component {
+  render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+    } = this.props;
+
+    return (
+      <div>
+        <h3 data-testid="name-card">{ cardName }</h3>
+        <img data-testid="image-card" src={ cardImage } alt={ cardName } />
+        <p data-testid="description-card">
+          { cardDescription }
+        </p>
+        <ul>
+          <li data-testid="attr1-card">
+            Card Attr1:
+            { cardAttr1 }
+          </li>
+          <li data-testid="attr2-card">
+            Card Attr2:
+            { cardAttr2 }
+          </li>
+          <li data-testid="attr3-card">
+            Card Attr3:
+            { cardAttr3 }
+          </li>
+        </ul>
+        <p data-testid="rare-card">
+          { cardRare }
+        </p>
+        {cardTrunfo
+          ? <span data-testid="trunfo-card">Super Trunfo</span>
+          : '' }
+      </div>
+    );
+  }
+}
+
+Card.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+};
+
+export default Card;
