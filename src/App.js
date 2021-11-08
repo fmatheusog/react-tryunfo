d6c9fd5e4ab0fa0114404a34ae64a5e6d080ec2c
Felipe Matheus <fmatheusog@gmail.com>
2021-11-08 13:46:17 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -17,10 +17,46 @@ class App extends Component {
       cardImage: '',
       cardRare: 'Normal',
       cardTrunfo: '',
-      isSaveButtonDisabled: false,
+      isSaveButtonDisabled: true,
+      errors: 0,
     };
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.handleValidation = this.handleValidation.bind(this);
+  }
+
+  handleValidation = () => {
+    const {
+      cardName,
+      cardDescription,
+      cardImage,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+    } = this.state;
+    const attrSum = Number(cardAttr1)
+      + Number(cardAttr2)
+      + Number(cardAttr3);
+    const attrLimit = 90;
+    const attrMaxLimit = 210;
+    const inputValidation = [cardName, cardDescription, cardImage]
+      .every((input) => input.length > 0) ? 0 : 1;
+    const attrValidation = [cardAttr1, cardAttr2, cardAttr3]
+      .every((attr) => attr >= 0 && attr <= attrLimit) ? 0 : 1;
+    const totalAttrValidation = attrSum <= attrMaxLimit ? 0 : 1;
+
+    const errors = inputValidation + attrValidation + totalAttrValidation;
+    console.log(errors);
+
+    if (errors > 0) {
+      this.setState({
+        isSaveButtonDisabled: true,
+      });
+    } else {
+      this.setState({
+        isSaveButtonDisabled: false,
+      });
+    }
   }
 
   onInputChange = (ev) => {
@@ -28,7 +64,7 @@ class App extends Component {
 
     this.setState({
       [ev.target.name]: value,
-    });
+    }, () => this.handleValidation());
   }
 
   render() {
